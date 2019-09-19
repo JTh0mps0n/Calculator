0fcc0de439ee76280d36c8aa3f6ee32b9a2f98d6
Justin Thompson <justindthomps@gmail.com>
2019-09-19 11:52:23 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Thompson, Justin D on 9/19/19.
+//  Copyright © 2019 Thompson, Justin D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
